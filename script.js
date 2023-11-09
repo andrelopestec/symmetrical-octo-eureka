f76c7eb00b9d03f027f0db724424c40ea949a5ea
andrelopestec <134522308+andrelopestec@users.noreply.github.com>
2023-11-08 23:48:16 -0300

    alteração

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
+
+// Função para mostrar um alerta
+function mostrarAlerta() {
+    alert("Alerta exibido!");
+}
+
+// Função para mudar a cor do texto do parágrafo
+function mudarCorTexto() {
+    document.getElementById("demo").style.color = "blue"; // Exemplo de mudança de cor
+}
+
+// Adicionando evento de mouseover ao parágrafo
+document.getElementById("demo").addEventListener("mouseover", mudarCorTexto);
